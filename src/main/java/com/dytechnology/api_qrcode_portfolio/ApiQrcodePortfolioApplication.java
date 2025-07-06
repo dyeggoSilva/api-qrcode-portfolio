de8745780378bb329e706027d2ddc2993b3f8a33
Dyeggo Bezerra <dyeggo0102@live.com>
2025-07-06 20:11:19 -0300

    :tada: init: Commit inicial

---
+++ src/main/java/com/dytechnology/api_qrcode_portfolio/ApiQrcodePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.dytechnology.api_qrcode_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQrcodePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQrcodePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dytechnology/api_qrcode_portfolio/ApiQrcodePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dytechnology.api_qrcode_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiQrcodePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
