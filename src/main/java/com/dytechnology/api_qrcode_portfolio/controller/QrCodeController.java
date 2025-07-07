406ffc6b3b960a2083b9103fe6b017b2ae5495a7
Dyeggo Bezerra <dyeggo0102@live.com>
2025-07-07 01:46:50 -0300

    :sparkles: feat: criação do QrCode Controller
    disponibilizando o endpoint de visualização do QrCode.

---
+++ src/main/java/com/dytechnology/api_qrcode_portfolio/controller/QrCodeController.java
@@ -0,0 +1,29 @@
+package com.dytechnology.api_qrcode_portfolio.controller;
+
+import com.dytechnology.api_qrcode_portfolio.service.QrCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+
+    @Autowired
+    private QrCodeService qrCodeService;
+
+    @GetMapping(produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> gerarQr(@RequestParam String texto) {
+        try {
+            byte[] imagem = qrCodeService.gerarQrCode(texto, 250, 250);
+            return ResponseEntity.ok(imagem);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    
+}
